510e65c1abc576830370fea0d11aba648108abe6
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2024-11-25 19:54:06 +0000

    +

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import styles from "./SearchBox.module.css";
 
-import { selectNameFilter } from "../../redux/filters/selectors";
+import { selectNameFilter } from "../../redux/filters/selectors.js";
 
 import { changeFilter } from "../../redux/filters/slice";
 
